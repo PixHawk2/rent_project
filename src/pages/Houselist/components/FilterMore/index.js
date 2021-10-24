7f6d35c22b1796e4337e82e85b1c83a465de08c2
hudi <wangqian1107@163.com>
2021-10-24 22:46:00 +0800

    filtermore动画实现

--- src/pages/Houselist/components/FilterMore/index.js
+++ src/pages/Houselist/components/FilterMore/index.js
@@ -1,6 +1,7 @@
-import { style } from "dom-helpers";
-import React from "react";
-import FilterFooter from "../../../../components/FilterFooter";
+import { style } from "dom-helpers"
+import React from "react"
+import FilterFooter from "../../../../components/FilterFooter"
+import { Spring,animated } from "react-spring"
 import styles from './index.module.css'
 
 
@@ -63,30 +64,48 @@ export default class FilterMore extends React.Component{
     
     render(){
         const {data:{roomType,oriented,floor,characteristic},onCancel,type} = this.props
+        const isShow = type === 'more'
         // console.log('-/-/-/-/-/-',type)
         return(
             <div className={styles.root}>
                 {/* 依然遮罩层 */}
-                <div className={styles.mask} onClick={() => onCancel(type)}/>
-                <div className={styles.tags}>
-                    <dl className={styles.dl}>
-                        <dt className={styles.dt}>户型</dt>
-                        <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
+                <Spring from={{opacity:0}} to={{opacity:isShow?1:0}}>
+                    {props =>{
+                        if(!isShow){
+                            return null
+                        }
+                        return (
+                            <animated.div style={props} className={styles.mask} onClick={() => onCancel(type)}/>
+                        )
+                    }}
+                </Spring>
+                <Spring  from={{opacity:0}} to={{opacity:isShow?1:0}}>
+                    { props =>{
+                        return (
+                            <animated.div>
+                                <div className={styles.tags}>
+                                    <dl className={styles.dl}>
+                                        <dt className={styles.dt}>户型</dt>
+                                        <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
 
-                        <dt className={styles.dt}>朝向</dt>
-                        <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
+                                        <dt className={styles.dt}>朝向</dt>
+                                        <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
 
-                        <dt className={styles.dt}>楼层</dt>
-                        <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
+                                        <dt className={styles.dt}>楼层</dt>
+                                        <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
 
-                        <dt className={styles.dt}>房屋亮点</dt>
-                        <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
+                                        <dt className={styles.dt}>房屋亮点</dt>
+                                        <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
 
-                    </dl>
-                    
-                </div>
-                {/* 底部按钮样式，并传入样式类,练习时底部按钮不显示是因为代码错误，需要调整bottom的值 */}
-                <FilterFooter className={styles.footer} cancelText='清除' onCancel={this.onClear} onOk={this.onConfirm}/>
+                                    </dl>
+                                 </div>
+                                {/* 底部按钮样式，并传入样式类,练习时底部按钮不显示是因为代码错误，需要调整bottom的值 */}
+                                <FilterFooter className={styles.footer} cancelText='清除' onCancel={this.onClear} onOk={this.onConfirm}/>
+                            </animated.div>
+                        )
+                    }}
+                </Spring>
+                
             </div>
         )
     }
